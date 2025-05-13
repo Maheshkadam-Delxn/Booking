b2bc4dd6573e544838aa3859f93c33bb591b42c2
Abhijeet-Ghode-DelXN <abhijeetghode.delxn@gmail.com>
2025-05-13 20:14:35 +0530

    change  by ghode

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -32,12 +32,12 @@ const AdminPage = () => {
 
   return (
     <AdminLayout>
-      <div className="mb-4 p-4 bg-gray-100 rounded">
+      {/* <div className="mb-4 p-4 bg-gray-100 rounded">
         <h2 className="text-lg font-semibold mb-2">Authentication Token:</h2>
         <div className="bg-white p-3 rounded border">
           <code className="break-all">{token}</code>
         </div>
-      </div>
+      </div> */}
       <Dashboard />
     </AdminLayout>
   );
